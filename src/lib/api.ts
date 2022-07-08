3ebcd2231cc0d84ad147b601dbdf1ad84c3f213f
Matheus [Kain] <mtheusmss@hotmail.com>
2022-07-08 12:10:50 -0300

    feat: firebase implementation

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,11 @@
-const API = "http://localhost:3333";
+const API = 'http://localhost:3333';
 
 const endpoint = (path: string): string => API + path;
 
 const get = async (path: string): Promise<any> => {
-  return fetch(endpoint(path)).then((res) => res.json());
+	return fetch(endpoint(path)).then((res) => res.json());
 };
 
 export const getVehicles = async () => {
-  return get("/vehicles");
+	return get('/vehicles');
 };
