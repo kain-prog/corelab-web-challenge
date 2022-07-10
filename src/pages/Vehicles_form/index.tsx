65f979db52d2ba7f74dda33988fdfa77af0f6924
Matheus [Kain] <mtheusmss@hotmail.com>
2022-07-10 04:37:53 -0300

    feat: edit vehicle form

--- src/pages/Vehicles_form/index.tsx
+++ src/pages/Vehicles_form/index.tsx
@@ -4,18 +4,43 @@ export default function VehiclesForm(){
 			<div className='container'>
 				<div className='row'>
 
-					<div className='col-md-6'>
+					<div className='col-md-6 my-3'>
 						<div className='d-flex flex-column'>
 							<label className='dark' htmlFor=''>NOME</label>
 							<input className='dark' type="text" name='' id='' />
 						</div>
 					</div>
 
-					<div className='col-md-6'>
+					<div className='col-md-6 my-3'>
 						<div className='d-flex flex-column'>
 							<label className='dark' htmlFor=''>MODELO</label>
 							<input className='dark' type="text" name='' id='' />
 						</div>
+					</div>
+
+					<div className='col-md-4 my-3'>
+						<div className='d-flex flex-column'>
+							<label className='dark' htmlFor=''>PREÇO</label>
+							<input className='dark' type="text" name='' id='' />
+						</div>
+					</div>
+
+					<div className='col-md-4 my-3'>
+						<div className='d-flex flex-column'>
+							<label className='dark' htmlFor=''>COR</label>
+							<input className='dark' type="text" name='' id='' />
+						</div>
+					</div>
+
+					<div className='col-md-4 my-3'>
+						<div className='d-flex flex-column'>
+							<label className='dark' htmlFor=''>ANO</label>
+							<input className='dark' type="text" name='' id='' />
+						</div>
+					</div>
+
+					<div className='col-md-2 text-end w-100 mt-4'>
+						<button className='btn border-dark'>Editar/Criar</button>
 					</div>
 
 				</div>
--- src/pages/Vehicles_id/index.tsx
+++ src/pages/Vehicles_id/index.tsx
@@ -1,6 +1,23 @@
-export default function VehiclesId(){
+import { useEffect } from 'react';
+import Title from '../../components/Title';
+import './index.scss';
+
+export default function Vehicles_id(){
+
+
+	useEffect(() => {
+		document.body.style.background = 'red';
+	  }, ['red']);
+
+
 	return(
 		<>
+			<div className='bg-all'>
+				<div className='container'>
+					<Title title='Detalhes do carro' />
+				</div>
+			</div>
+
 		</>
 	);
 };
