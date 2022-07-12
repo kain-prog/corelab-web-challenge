7d0b39a12c2377d2590bf01d694c8df9688375c6
Matheus [Kain] <mtheusmss@hotmail.com>
2022-07-12 00:15:45 -0300

    feat: ending project

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -25,10 +25,10 @@ export default function Filter(props: any){
 							<label className='light' htmlFor=''>PREÇO</label>
 							<div className='row'>
 								<div className='col-md-6'>
-									<input className='light' type='text' name='' id='' placeholder='min.' />
+									<input className='light' type='text' name='' id='' placeholder='min.' disabled />
 								</div>
 								<div className='col-md-6'>
-									<input className='light' type='text' name='' id='' placeholder='max.' />
+									<input className='light' type='text' name='' id='' placeholder='max.' disabled />
 								</div>
 							</div>
 						</div>
@@ -54,7 +54,7 @@ export default function Filter(props: any){
 					<div className='col-md-3'>
 						<div className='d-flex flex-column'>
 							<label className='light' htmlFor=''>MODELO</label>
-							<select className='light' name='color' id=''>
+							<select className='light' name='color' id='' disabled >
 								<option value=''>Renault</option>
 								<option value=''>Honda</option>
 							</select>
@@ -64,7 +64,7 @@ export default function Filter(props: any){
 					<div className='col-md-3'>
 						<div className='d-flex flex-column'>
 							<label className='light' htmlFor=''>ANO</label>
-							<select className='light' name='color' id=''>
+							<select className='light' name='color' id='' disabled>
 								<option value='#'>2013</option>
 								<option value='#'>2015</option>
 							</select>
@@ -73,7 +73,7 @@ export default function Filter(props: any){
 				</div>
 				<div className='row'>
 					<div className='mt-4 col-12 d-flex justify-content-between'>
-						<p>50 resultados</p>
+						<p></p>
 						<button className='btn btn-outline-light'>Limpar</button>
 					</div>
 				</div>
